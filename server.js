de90b37e2fc649f3db250c292fe7fee55b312bcd
tonkao <napatr.sara@gmail.com>
2023-04-25 20:06:56 +0700

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Route files
 const campgrounds = require('./routes/campgrounds')
@@ -7,10 +8,20 @@ const campgrounds = require('./routes/campgrounds')
 // Load env vars
 dotenv.config({path:'./config/config.env'});
 
+// Connect to database
+connectDB();
+
 const app=express();
 
 // Mount routers
 app.use('/api/v1/campgrounds', campgrounds);
 
 const PORT = process.env.PORT;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV , 'mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV , 'mode on port ', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
